89e7d77f9374e846c9d00fc37a305407c2039902
adam <brown-a2@users.noreply.github.com>
2026-06-16 22:28:57 +0100

    Fix bugs with deploy

--- bin/deploy-local.sh
+++ bin/deploy-local.sh
@@ -14,15 +14,19 @@ fi
 # - installs the chart if it doesn't exist
 # - applies the correct environment values
 # - creates the namespace if it doesn't exist
-# - will timeout if there is any issue after 60 seconds
+# - will time out if there is any issue after 15 minutes
 # - uses atomic flag which rolls back changes in the case of a failed upgrade
+#
+# NOTE: timeout must exceed the worst-case cold image pull. A short timeout
+# combined with --atomic rolls back a deploy that is merely waiting on a slow
+# first-time image pull, which then "works" on retry once the image is cached.
 #
 helm upgrade wordpress helm_deploy/wordpress \
     --install \
     --values helm_deploy/wordpress/values-local.yaml \
     --namespace hale-platform-local \
     --create-namespace \
-    --timeout 5m \
+    --timeout 15m \
     --atomic
 
 set -e
--- prod/wordpress.dockerfile
+++ prod/wordpress.dockerfile
@@ -7,7 +7,7 @@ ENV PHP_INI_DIR=/usr/local/etc/php
 # Install additional Alpine packages and wp-cli
 RUN set -eux; \
     apk update && \
-    apk add --no-cache less vim mariadb-client mariadb-backup htop libjpeg-turbo-utils && \
+    apk add --no-cache less mariadb-client libjpeg-turbo-utils && \
     curl -o /usr/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
     chmod +x /usr/bin/wp && \
     addgroup -g 1001 wp && \
